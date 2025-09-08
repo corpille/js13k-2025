eddc973d8f1b6ec3a0327f27e5d4767eac302ddb
Corentin Pillet <corentin.pillet@energiency.com>
2025-09-08 13:09:45 +0200

    Fixing wolr 1-4 and fixing transition replay

--- src/scenes/transitionScene.ts
+++ src/scenes/transitionScene.ts
@@ -6,7 +6,7 @@ import { UiScene } from '../ui-elements/Scene';
 import { UiButton } from '../ui-elements/UiButton';
 import { UiList } from '../ui-elements/UiList';
 import { UiText } from '../ui-elements/UiText';
-import { FPS, getGridRealHeight, getGridRealWidth, getMoveSpeed, getSquareSize, gridWidth } from '../config';
+import { FPS, getGridRealHeight, getGridRealWidth, getMoveSpeed, gridWidth } from '../config';
 import PlayerEntity from '../entities/PlayerEntity';
 import BlocEntity from '../entities/BlocEntity';
 import { sleep } from '../utils';
@@ -27,7 +27,7 @@ async function defautlCallback(player: PlayerEntity, aeter: Aether) {
   Game.instance.reset();
 }
 
-async function preLoad(player: PlayerEntity, aeter: Aether, ground: BlocEntity) {
+async function preLoad(player: PlayerEntity, aether: Aether, ground: BlocEntity) {
   await moveCat(player, getGridRealWidth() / 2 - player.width / 2);
 }
 
@@ -38,13 +38,13 @@ export function getTransitionScene(
   onloadCallback = preLoad,
 ) {
   const player = new PlayerEntity(-2, 1);
-  const aeter = new Aether(-2, 1);
+  const aether = new Aether(-2, 1);
   const ground = new BlocEntity(0, 0, gridWidth, 1, true);
 
   const scene = new UiScene(true);
   scene.onRender = (ctx: CanvasRenderingContext2D) => {
     player.render(ctx);
-    aeter.render(ctx);
+    aether.render(ctx);
     ground.render(ctx);
   };
   const list = new UiList(0, 0, [true, false]);
@@ -55,14 +55,18 @@ export function getTransitionScene(
   list.y = (getGridRealHeight() - list.getRealSize().height) / 2;
   list.x = (getGridRealWidth() - list.getRealSize().width) / 2;
 
-  button.onClick = async () => await buttonCallback(player, aeter);
+  button.onClick = async () => await buttonCallback(player, aether);
   scene.add(list);
 
   scene.onLoad = async () => {
     scene.autoRefresh = true;
+    player.x = -2;
+    player.offsets.x = 0;
+    aether.x = -2;
+    aether.offsets.x = 0;
     list.inverted = Game.instance.currentLvl > backgroundShift ? 0 : 1;
     list.elements = [];
-    await onloadCallback(player, aeter, ground);
+    await onloadCallback(player, aether, ground);
     await slowDisplayText(list, msg);
     list.add(button);
   };
